fbdf149065edcd4c64f976bd31ae4fdcc2461853
erick <erick_olazabal@usmp.pe>
2020-10-08 08:02:36 -0500

    cambio

---
+++ Assets/Scripts/jugador.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jugador : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    public float maxSpeed;
+    public GameObject referencia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moverHorizontal = Input.GetAxis("Horizontal");
+        float moverVertical = Input.GetAxis("Vertical");
+
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+
+        rb.AddForce(moverVertical * referencia.transform.forward * speed);
+        rb.AddForce(moverHorizontal * referencia.transform.right * speed);
+    }
+}
